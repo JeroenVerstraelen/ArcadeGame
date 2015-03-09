e082dc1cf7c771e339b84954244a32c64fd28ccf
JeroenVerstraelen <jeroenverstraelen@hotmail.com>
2015-03-09 23:12:03 +0100

    more

---
+++ spec1.0/spec1.0/ArcadeGameTests.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : ArcadeGameTests.cpp
+// Author      : Jeroen Verstraelen
+// Version     :
+// Description : ArcadeGame in C++
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+#include "TicTacToe.h"
+
+class ArcadeGameTest: public ::testing::Test {
+protected:
+
+	virtual void SetUp() {
+
+	}
+
+	virtual void TearDown() {
+
+	}
+
+	/**
+	Declares the variables your tests want to use.
+	*/
+	//TicTacToe ttt_;
+}
