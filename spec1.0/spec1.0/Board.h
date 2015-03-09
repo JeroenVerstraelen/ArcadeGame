ad5284f1074c61a420c0e97b5ea6e89a71cdf3ee
JeroenVerstraelen <jeroenverstraelen@hotmail.com>
2015-03-10 00:17:26 +0100

    more

---
+++ spec1.0/spec1.0/Board.h
@@ -0,0 +1,17 @@
+
+#ifndef Base3D_CC
+#define Base3D_CC
+
+class Board{
+private:
+	// entities includes player
+	vector<Entity*> entities;
+	Player player;
+public:
+	//Default constructor
+	Board();
+
+};
+
+
+#endif
